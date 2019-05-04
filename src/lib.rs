5f234ad72ee0f6b0a14c4a16b4c64047a69c69a1
Samuel Ortiz <sameo@linux.intel.com>
2019-05-04 10:48:42 +0200

    vm-virtio: Fix licensing
    Add the Apache-2.0 and BSD-3-Clause licenses for this crate, and
    dual license Apache-2.0 OR/AND BSD-3-Clause.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
 //
 // Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the THIRD-PARTY file.
+// found in the LICENSE-BSD-3-Clause file.
+//
+// SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
 #![deny(missing_docs)]
 
 //! Implements virtio devices, queues, and transport mechanisms.
--- src/queue.rs
+++ src/queue.rs
@@ -1,9 +1,10 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
 //
 // Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the THIRD-PARTY file.
+// found in the LICENSE-BSD-3-Clause file.
+//
+// SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
 
 use std::cmp::min;
 use std::mem::size_of;
