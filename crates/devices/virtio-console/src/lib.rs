210964a5b366d8c1565667a0fb130882361aee25
Niculae Radu <r.niculae99@gmail.com>
2022-05-26 16:57:04 +0300

    Virtio console: documentation and error handling
    Added documentation and improved the error handling
    of the virtio console.
    
    Signed-off-by: Niculae Radu <r.niculae99@gmail.com>

--- crates/devices/virtio-console/src/lib.rs
+++ crates/devices/virtio-console/src/lib.rs
@@ -1,8 +1,11 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
 //! A crate that provides console device specific components as described
 //! by the virtio specification.
 
 #![deny(missing_docs)]
 
-/// Console module
+/// Contains an abstraction for the virtio console descriptor chain handling.
 pub mod console;
