5fb43d4fa513ede860320b3968fdd757c0cfbbac
Wenyu Huang <huangwenyuu@outlook.com>
2024-10-20 10:51:32 -0400

    Add packed descriptor
    Add the packed descriptor for support packed vring.
    
    Signed-off-by: Wenyu Huang <huangwenyuu@outlook.com>

--- virtio-queue/src/desc/mod.rs
+++ virtio-queue/src/desc/mod.rs
@@ -2,6 +2,7 @@
 
 use vm_memory::{ByteValued, Le16, Le32, Le64};
 
+pub mod packed;
 pub mod split;
 
 /// a virtio descriptor
@@ -33,9 +34,26 @@ impl From<split::Descriptor> for RawDescriptor {
     }
 }
 
+impl From<packed::Descriptor> for RawDescriptor {
+    fn from(desc: packed::Descriptor) -> Self {
+        RawDescriptor(
+            Le64::from(desc.addr().0),
+            Le32::from(desc.len()),
+            Le16::from(desc.id()),
+            Le16::from(desc.flags()),
+        )
+    }
+}
+
 impl From<RawDescriptor> for split::Descriptor {
     fn from(desc: RawDescriptor) -> split::Descriptor {
         split::Descriptor::new(desc.0.into(), desc.1.into(), desc.2.into(), desc.3.into())
+    }
+}
+
+impl From<RawDescriptor> for packed::Descriptor {
+    fn from(desc: RawDescriptor) -> packed::Descriptor {
+        packed::Descriptor::new(desc.0.into(), desc.1.into(), desc.2.into(), desc.3.into())
     }
 }
 
@@ -43,7 +61,7 @@ impl From<RawDescriptor> for split::Descriptor {
 mod tests {
     use vm_memory::{Le16, Le32, Le64};
 
-    use super::{split, RawDescriptor};
+    use super::{packed, split, RawDescriptor};
 
     #[test]
     fn test_desc_from_split() {
@@ -63,5 +81,25 @@ mod tests {
         assert_eq!(split_desc.len(), desc.1);
         assert_eq!(split_desc.flags(), desc.2);
         assert_eq!(split_desc.next(), desc.3);
+    }
+
+    #[test]
+    fn test_desc_from_packed() {
+        let packed_desc = packed::Descriptor::new(1, 2, 3, 4);
+        let desc = RawDescriptor::from(packed_desc);
+        assert_eq!(packed_desc.addr().0, desc.0);
+        assert_eq!(packed_desc.len(), desc.1);
+        assert_eq!(packed_desc.id(), desc.2);
+        assert_eq!(packed_desc.flags(), desc.3);
+    }
+
+    #[test]
+    fn test_packed_from_desc() {
+        let desc = RawDescriptor(Le64::from(1), Le32::from(2), Le16::from(3), Le16::from(4));
+        let packed_desc = packed::Descriptor::from(desc);
+        assert_eq!(packed_desc.addr().0, desc.0);
+        assert_eq!(packed_desc.len(), desc.1);
+        assert_eq!(packed_desc.id(), desc.2);
+        assert_eq!(packed_desc.flags(), desc.3);
     }
 }
